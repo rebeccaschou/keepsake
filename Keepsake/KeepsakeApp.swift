ad9a7a559f5bd2372e3533156b60828a2c10a340
rebeccaschou <rstchou@gmail.com>
2026-04-08 16:44:08 -0400

    Initial Commit

---
+++ Keepsake/KeepsakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepsakeApp.swift
+//  Keepsake
+//
+//  Created by Rebecca Chou on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct KeepsakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
